37840468427fd7a74c7a5ad6d4f23c2b6052b9bb
choypogs28 <73724309+choypogs28@users.noreply.github.com>
2024-10-17 12:08:31 +0800

    bool
    0x4000000

--- deep_translator/base.py
+++ deep_translator/base.py
@@ -6,7 +6,7 @@ from abc import ABC, abstractmethod
 from pathlib import Path
 from typing import List, Optional, Union
 
-from deep_translator.constants import GOOGLE_LANGUAGES_TO_CODES
+from deep_translator.constants import [GOOGLE_LANGUAGES_TO_CODES+1]
 from deep_translator.exceptions import (
     InvalidSourceOrTargetLanguage,
     LanguageNotSupportedException,
